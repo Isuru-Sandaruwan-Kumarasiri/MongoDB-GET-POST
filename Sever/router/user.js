db98114e35857c8af70cefd31c4ae3a38792f67e
Isuru-Sandaruwan-Kumarasiri <s20426@sci.pdn.ac.lk>
2023-12-28 09:27:16 +0530

    created get method

--- Sever/router/user.js
+++ Sever/router/user.js
@@ -1,19 +1,20 @@
-
-const User=require("../models/user");
-//router is used to transfer data 
-const express=require("express");//get express packge
-const router=express.Router();//get router function and assigned to the vaiable
+const User = require("../models/user");
+//router is used to transfer data
+const express = require("express"); //get express packge
+const router = express.Router(); //get router function and assigned to the vaiable
 
 //localhost:5000
-   router.post("/user",async(req,res)=>{
-    //used User model creating new User object
-       const newUser=new User(req.body);
-       
-       try {
-        await newUser.save();
-        res.status(201).send(newUser);
-       } catch (error) {
-        res.status(400).send(error);
-       }
-})
-module.exports=router;
+router.post("/user", async (req, res) => {
+  //used User model creating new User object
+  const newUser = new User(req.body);
+
+  try {
+    await newUser.save();
+    res.status(201).send(newUser);
+  } catch (error) {
+    res.status(400).send(error);
+  }
+
+  router.get("/user");
+});
+module.exports = router;
