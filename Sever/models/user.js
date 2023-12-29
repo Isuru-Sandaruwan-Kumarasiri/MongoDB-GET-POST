ed3f118e0819670d6649eee66791732a894bce03
Isuru-Sandaruwan-Kumarasiri <s20426@sci.pdn.ac.lk>
2023-12-29 23:43:22 +0530

    create static method using user model and create loging router

--- Sever/models/user.js
+++ Sever/models/user.js
@@ -1,3 +1,4 @@
+const bcrypt = require("bcryptjs/dist/bcrypt");
 const mongoose=require("mongoose");
 
 //to craete template use schema class
@@ -19,8 +20,49 @@ const userShema=new Shema({
         age :{
             type:Number,
             required:true
+        },
+        password:{
+            type:String,
+            trim:true,
+            required:true,
         }
-})
+});
+
+userShema.pre("save",async function(next){
+    //.pre--->user save karnn kalin  monwad krann oni function run karnne ,save func ekat kalin
+    //mona function ekad kiyala parameter ekak vidiyat denn oni"save()"
+    //next--->next step ekat yannn kiyala order eka denne
+
+    const user=this;//save karn user attributs okkom assigned karna variable ekt
+
+    if(user.isModified("password")){
+        user.password=await bcrypt.hash(user.password,8) //pasword=password+1 like this line/8-algorithem eka run wen ganana
+    }
+
+});
+
+userShema.statics.findByCredentials=async(email,password)=>{
+    //user kenekw create karn natuw user collection eka access karnn hadana method ekak
+    //mehidi user use karla findone method walin password ,email check karana eka
+
+    const user=await User.findOne({email})//{email} means {email:email}
+
+    if (!user){
+        throw new Error;
+    }
+    const isMatch=await bcrypt.compare(password,user.password);
+
+    if(!isMatch){
+        throw new Error;
+    }
+
+    return user;
+}
+
+
+
+
+
 
 //using template ,create model
 const User=mongoose.model("User",userShema);
